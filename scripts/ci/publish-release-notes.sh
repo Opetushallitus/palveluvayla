30dbbfe1184433e9e776af7ed0bfb14a2f2e692d
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-10-24 13:51:55 +0300

    fix the script location now that the repo location is fixed

--- scripts/ci/publish-release-notes.sh
+++ scripts/ci/publish-release-notes.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 readonly repo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )"
-source "${repo}/../lib/common-functions.sh"
+source "${repo}/scripts/lib/common-functions.sh"
 
 function main {
   local -r env=$(parse_env_from_script_name)
